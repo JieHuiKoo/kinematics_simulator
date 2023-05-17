ddc07091b5f9136b719e7797778555199ea3d0d4
JieHuiKoo <31171083+JieHuiKoo@users.noreply.github.com>
2023-05-18 07:02:49 +0800

    Tested OpenCV

---
+++ build/CMakeFiles/kinematics_simulator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kinematics_simulator.dir/main.cpp.o"
+  "CMakeFiles/kinematics_simulator.dir/main.cpp.o.d"
+  "kinematics_simulator"
+  "kinematics_simulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kinematics_simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/kinematics_simulator.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for kinematics_simulator.
+# This may be replaced when dependencies are built.
