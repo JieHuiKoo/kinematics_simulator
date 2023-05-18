9146f27e555662e0f4b792005c9287291ec6b5e1
JieHuiKoo <31171083+JieHuiKoo@users.noreply.github.com>
2023-05-18 10:38:37 +0800

    Removed unneeded template code

--- main.cpp
+++ main.cpp
@@ -18,13 +18,10 @@
 //     waitKey(0);
 //     return 0;
 // }
-// Window.h
 #include <SDL.h>
 #include <iostream>
-#include "include/Window.h"
 
 int main() {
-  Window AppWindow;
   auto KeyboardState = SDL_GetKeyboardState(nullptr);
   SDL_Event Event;
   while(true) {
@@ -37,27 +34,26 @@ int main() {
 
       // Keyboard Input
       else if (Event.type == SDL_KEYDOWN) {
-        if (Event.key.keysym.sym == SDLK_UP) {
-          AppWindow.MoveRelative(0, -10);
-        } else if (Event.key.keysym.sym == SDLK_DOWN) {
-          AppWindow.MoveRelative(0, 10);
-        } else if (Event.key.keysym.sym == SDLK_LEFT) {
-          AppWindow.MoveRelative(-10, 0);
-        } else if (Event.key.keysym.sym == SDLK_RIGHT) {
-          AppWindow.MoveRelative(10, 0);
-        } else if (Event.key.keysym.sym == SDLK_RETURN) {
-          int x, y;
-          Uint32 Buttons { SDL_GetMouseState(&x, &y) };
-
-          std::cout << "Mouse is at " << x << ", " << y;
-          if ((Buttons & SDL_BUTTON_LMASK)) {
-            std::cout << " - Left Button is pressed";
-          }
-          if ((Buttons & SDL_BUTTON_RMASK)) {
-            std::cout << " - Right Button is pressed";
-          }
-          std::cout << "\n";
-        } else {
+        if (Event.key.keysym.sym == SDLK_UP) 
+        {
+          // Key Press Up
+        } 
+        
+        else if (Event.key.keysym.sym == SDLK_DOWN) 
+        {
+          // Key Press Down
+        } 
+        
+        else if (Event.key.keysym.sym == SDLK_LEFT) 
+        {
+          // Key Press Left
+        } 
+        
+        else if (Event.key.keysym.sym == SDLK_RIGHT) 
+        {
+          // Key Press Right
+        }
+        else {
           std::cout
             << "Key Pressed! Key Code: "
             << Event.key.keysym.sym
@@ -67,7 +63,5 @@ int main() {
         }
       }
     }
-    AppWindow.Update();
-    AppWindow.RenderFrame();
   }
 }
