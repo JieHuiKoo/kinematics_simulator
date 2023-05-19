5260717db849a9567e84b38a8a4487a65c53a8d5
JieHuiKoo <31171083+JieHuiKoo@users.noreply.github.com>
2023-05-19 20:17:02 +0800

    Draw vehicle model with rotation

--- include/Vehicle.h
+++ include/Vehicle.h
@@ -6,52 +6,32 @@ class Vehicle {
 public:
     void DrawPosition(cv::Mat *map) {
         
-        PoseFrame vehCentre_in_globalMap1 = TransformFromVehFrameToGlobalMapFrame(this->centreIndicator_in_vehFrame);
-        PoseFrame vehLeftIndicator_in_globalMap1 = TransformFromVehFrameToGlobalMapFrame(this->leftIndicator_in_vehFrame);
-        PoseFrame vehRightIndicator_in_globalMap1 = TransformFromVehFrameToGlobalMapFrame(this->rightIndicator_in_vehFrame);
-        PoseFrame vehForwardIndicator_in_globalMap1 = TransformFromVehFrameToGlobalMapFrame(this->forwardIndicator_in_vehFrame);
-        PoseFrame vehBackwardIndicator_in_globalMap1 = TransformFromVehFrameToGlobalMapFrame(this->backwardIndicator_in_vehFrame);
-        PoseFrame TOFsensor1_in_globalMap1 = TransformFromVehFrameToGlobalMapFrame(this->TOFsensor1_in_vehFrame);
+        PoseFrame vehCentre_in_globalMap = TransformFromVehFrameToGlobalMapFrame(this->centreIndicator_in_vehFrame);
+        PoseFrame vehLeftIndicator_in_globalMap = TransformFromVehFrameToGlobalMapFrame(this->leftIndicator_in_vehFrame);
+        PoseFrame vehRightIndicator_in_globalMap = TransformFromVehFrameToGlobalMapFrame(this->rightIndicator_in_vehFrame);
+        PoseFrame vehForwardIndicator_in_globalMap = TransformFromVehFrameToGlobalMapFrame(this->forwardIndicator_in_vehFrame);
+        PoseFrame vehBackwardIndicator_in_globalMap = TransformFromVehFrameToGlobalMapFrame(this->backwardIndicator_in_vehFrame);
+        PoseFrame TOFsensor1_in_globalMap = TransformFromVehFrameToGlobalMapFrame(this->TOFsensor1_in_vehFrame);
 
-        cv::Point veh_position_in_OpenCVMap1 = ConvertMapToOpenCVPoint(cv::Point(this->veh_pose_in_globalMap.x, this->veh_pose_in_globalMap.y));
-        cv::Point vehCentreIndicator_in_OpenCVMap1 = ConvertMapToOpenCVPoint(cv::Point(vehCentre_in_globalMap1.x, vehCentre_in_globalMap1.y));
-        cv::Point vehLeftIndicator_in_OpenCVMap1 = ConvertMapToOpenCVPoint(cv::Point(vehLeftIndicator_in_globalMap1.x, vehLeftIndicator_in_globalMap1.y));
-        cv::Point vehRightIndicator_in_OpenCVMap1 = ConvertMapToOpenCVPoint(cv::Point(vehRightIndicator_in_globalMap1.x, vehRightIndicator_in_globalMap1.y));
-        cv::Point vehForwardIndicator_in_OpenCVMap1 = ConvertMapToOpenCVPoint(cv::Point(vehForwardIndicator_in_globalMap1.x, vehForwardIndicator_in_globalMap1.y));
-        cv::Point vehBackwardIndicator_in_OpenCVMap1 = ConvertMapToOpenCVPoint(cv::Point(vehBackwardIndicator_in_globalMap1.x, vehBackwardIndicator_in_globalMap1.y));
-        cv::Point TOFsensor1_in_OpenCVMap1 = ConvertMapToOpenCVPoint(cv::Point(TOFsensor1_in_globalMap1.x, TOFsensor1_in_globalMap1.y));
+        cv::Point veh_position_in_OpenCVMap = ConvertMapToOpenCVPoint(cv::Point(this->veh_pose_in_globalMap.x, this->veh_pose_in_globalMap.y));
+        cv::Point vehCentreIndicator_in_OpenCVMap = ConvertMapToOpenCVPoint(cv::Point(vehCentre_in_globalMap.x, vehCentre_in_globalMap.y));
+        cv::Point vehLeftIndicator_in_OpenCVMap = ConvertMapToOpenCVPoint(cv::Point(vehLeftIndicator_in_globalMap.x, vehLeftIndicator_in_globalMap.y));
+        cv::Point vehRightIndicator_in_OpenCVMap = ConvertMapToOpenCVPoint(cv::Point(vehRightIndicator_in_globalMap.x, vehRightIndicator_in_globalMap.y));
+        cv::Point vehForwardIndicator_in_OpenCVMap = ConvertMapToOpenCVPoint(cv::Point(vehForwardIndicator_in_globalMap.x, vehForwardIndicator_in_globalMap.y));
+        cv::Point vehBackwardIndicator_in_OpenCVMap = ConvertMapToOpenCVPoint(cv::Point(vehBackwardIndicator_in_globalMap.x, vehBackwardIndicator_in_globalMap.y));
+        cv::Point TOFsensor1_in_OpenCVMap = ConvertMapToOpenCVPoint(cv::Point(TOFsensor1_in_globalMap.x, TOFsensor1_in_globalMap.y));
         
-        cv::circle(*map, veh_position_in_OpenCVMap1, 4, cv::Scalar(255, 255, 255), -1);
-        cv::circle(*map, vehCentreIndicator_in_OpenCVMap1, 4, cv::Scalar(255, 255, 255), -1);
-        cv::circle(*map, vehLeftIndicator_in_OpenCVMap1, 4, cv::Scalar(255, 255, 255), -1);
-        cv::circle(*map, vehRightIndicator_in_OpenCVMap1, 4, cv::Scalar(255, 255, 255), -1);
-        cv::circle(*map, vehForwardIndicator_in_OpenCVMap1, 4, cv::Scalar(255, 255, 255), -1);
-        cv::circle(*map, vehBackwardIndicator_in_OpenCVMap1, 4, cv::Scalar(255, 255, 255), -1);
-        cv::circle(*map, TOFsensor1_in_OpenCVMap1, 4, cv::Scalar(255, 255, 255), -1);
+        cv::circle(*map, veh_position_in_OpenCVMap, 4, cv::Scalar(255, 255, 255), -1);
+        cv::circle(*map, vehCentreIndicator_in_OpenCVMap, 4, cv::Scalar(255, 255, 255), -1);
+        cv::circle(*map, vehLeftIndicator_in_OpenCVMap, 4, cv::Scalar(255, 255, 255), -1);
+        cv::circle(*map, vehRightIndicator_in_OpenCVMap, 4, cv::Scalar(255, 255, 255), -1);
+        cv::circle(*map, vehForwardIndicator_in_OpenCVMap, 4, cv::Scalar(255, 255, 255), -1);
+        cv::circle(*map, vehBackwardIndicator_in_OpenCVMap, 4, cv::Scalar(255, 255, 255), -1);
+        cv::circle(*map, TOFsensor1_in_OpenCVMap, 4, cv::Scalar(255, 255, 255), -1);
 
-        // PoseFrame vehCentre_in_globalMap = TransformFromVehFrameToGlobalMapFrame1(this->centreIndicator_in_vehFrame);
-        // PoseFrame vehLeftIndicator_in_globalMap = TransformFromVehFrameToGlobalMapFrame1(this->leftIndicator_in_vehFrame);
-        // PoseFrame vehRightIndicator_in_globalMap = TransformFromVehFrameToGlobalMapFrame1(this->rightIndicator_in_vehFrame);
-        // PoseFrame vehForwardIndicator_in_globalMap = TransformFromVehFrameToGlobalMapFrame1(this->forwardIndicator_in_vehFrame);
-        // PoseFrame vehBackwardIndicator_in_globalMap = TransformFromVehFrameToGlobalMapFrame1(this->backwardIndicator_in_vehFrame);
-        // PoseFrame TOFsensor1_in_globalMap = TransformFromVehFrameToGlobalMapFrame1(this->TOFsensor1_in_vehFrame);
-
-        // cv::Point veh_position_in_OpenCVMap = ConvertMapToOpenCVPoint(cv::Point(this->veh_pose_in_globalMap.x, this->veh_pose_in_globalMap.y));
-        // cv::Point vehCentreIndicator_in_OpenCVMap = ConvertMapToOpenCVPoint(cv::Point(vehCentre_in_globalMap.x, vehCentre_in_globalMap.y));
-        // cv::Point vehLeftIndicator_in_OpenCVMap = ConvertMapToOpenCVPoint(cv::Point(vehLeftIndicator_in_globalMap.x, vehLeftIndicator_in_globalMap.y));
-        // cv::Point vehRightIndicator_in_OpenCVMap = ConvertMapToOpenCVPoint(cv::Point(vehRightIndicator_in_globalMap.x, vehRightIndicator_in_globalMap.y));
-        // cv::Point vehForwardIndicator_in_OpenCVMap = ConvertMapToOpenCVPoint(cv::Point(vehForwardIndicator_in_globalMap.x, vehForwardIndicator_in_globalMap.y));
-        // cv::Point vehBackwardIndicator_in_OpenCVMap = ConvertMapToOpenCVPoint(cv::Point(vehBackwardIndicator_in_globalMap.x, vehBackwardIndicator_in_globalMap.y));
-        // cv::Point TOFsensor1_in_OpenCVMap = ConvertMapToOpenCVPoint(cv::Point(TOFsensor1_in_globalMap.x, TOFsensor1_in_globalMap.y));
-        
-        // cv::circle(*map, veh_position_in_OpenCVMap, 4, cv::Scalar(255, 255, 255), -1);
-        // cv::circle(*map, vehCentreIndicator_in_OpenCVMap, 4, cv::Scalar(0, 255, 255), -1);
-        // cv::circle(*map, vehLeftIndicator_in_OpenCVMap, 4, cv::Scalar(0, 0, 255), -1);
-        // cv::circle(*map, vehRightIndicator_in_OpenCVMap, 4, cv::Scalar(255, 255, 255), -1);
-        // cv::circle(*map, vehForwardIndicator_in_OpenCVMap, 4, cv::Scalar(0, 255, 255), -1);
-        // cv::circle(*map, vehBackwardIndicator_in_OpenCVMap, 4, cv::Scalar(0, 255, 255), -1);
-        // cv::circle(*map, TOFsensor1_in_OpenCVMap, 4, cv::Scalar(255, 0, 0), -1);
-
+        cv::Size veh_size = cv::Size(this->veh_length, this->veh_width);
+        std::cout << vehCentre_in_globalMap.orientation << std::endl;
+        DrawRotatedRect(map, vehCentreIndicator_in_OpenCVMap, veh_size, vehCentre_in_globalMap.orientation);
     }
     
     void UpdatePosition(double scale) {
@@ -78,15 +58,15 @@ public:
         this->veh_width = veh_width*scale; 
         this->veh_pose_in_globalMap = veh_pose_in_globalMap;
 
-        this->TOFsensor1_in_vehFrame.x = veh_length*0.8*scale;
+        this->TOFsensor1_in_vehFrame.x = veh_length*0.7*scale;
         this->TOFsensor1_in_vehFrame.y = 0;
         this->TOFsensor1_in_vehFrame.orientation = 0;
 
-        this->leftIndicator_in_vehFrame.x = veh_length*0.8*scale;
+        this->leftIndicator_in_vehFrame.x = veh_length*0.7*scale;
         this->leftIndicator_in_vehFrame.y = -veh_width/4*scale;
         this->leftIndicator_in_vehFrame.orientation = 0;
 
-        this->rightIndicator_in_vehFrame.x = veh_length*0.8*scale;
+        this->rightIndicator_in_vehFrame.x = veh_length*0.7*scale;
         this->rightIndicator_in_vehFrame.y = veh_width/4*scale;
         this->rightIndicator_in_vehFrame.orientation = 0;
 
@@ -156,7 +136,7 @@ private:
         PoseFrame input_poseFrame_in_globalMap(
             dist_from_point_of_rotation*sin(-this->veh_pose_in_globalMap.orientation + angle_offset)+this->veh_pose_in_globalMap.x, 
             dist_from_point_of_rotation*cos(-this->veh_pose_in_globalMap.orientation + angle_offset)+this->veh_pose_in_globalMap.y, 
-            input_frame.orientation+veh_pose_in_globalMap.orientation);
+            input_frame.orientation-veh_pose_in_globalMap.orientation);
 
         return input_poseFrame_in_globalMap;
     }
@@ -175,6 +155,16 @@ private:
     double CalculateEuclideanDist(cv::Point pt1, cv::Point pt2)
     {
         return sqrt((pow((pt1.x-pt2.x),2)) + (pow((pt1.y-pt2.y),2)));
+    }
+
+    void DrawRotatedRect(cv::Mat *image, cv::Point center, cv::Size size, double rotation_Radians)
+    {
+        double rotation_degrees = rotation_Radians*180/M_PI;
+        cv::RotatedRect rRect = cv::RotatedRect(center, size, rotation_degrees);
+        cv::Point2f vertices[4];
+        rRect.points(vertices);
+        for (int i = 0; i < 4; i++)
+            cv::line(*image, vertices[i], vertices[(i+1)%4], cv::Scalar(0,255,0), 2);
     }
 
     // TODO:
